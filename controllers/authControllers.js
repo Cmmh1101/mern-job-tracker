d8643900cdc0b4f2a40ff57cc5a5491e6c2fcc72
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-05-22 22:52:07 -0500

    create user and jwt

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -16,7 +16,19 @@ const register = async (req, res) => {
   }
 
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      name: user.name,
+      lastName: user.lastName,
+      location: user.location,
+    },
+    token,
+    location: user.location,
+  });
 };
 
 const login = async (req, res) => {
