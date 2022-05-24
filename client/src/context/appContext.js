febe81a26e2373f18964342d5bdbc54349ebfa8b
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-05-23 23:35:11 -0500

    saving, getting and deleting from localstorage

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -11,15 +11,19 @@ import {
   REGISTER_USER_ERROR,
 } from "./actions";
 
+const token = localStorage.getItem("token");
+const user = localStorage.getItem("user");
+const userLocation = localStorage.getItem("location");
+
 const initialState = {
-  user: null,
-  token: null,
-  userLocation: "",
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  userLocation: userLocation || "",
   isLoading: false,
   showAlert: false,
   alertText: "",
   alertType: "",
-  jobLocation: "",
+  jobLocation: userLocation || "",
 };
 
 const AppContext = createContext();
@@ -42,6 +46,18 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
+  const addUserToLocalStorage = async ({ user, token, location }) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+    localStorage.setItem("location", location);
+  };
+
+  const removeUserFromLocalStorage = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("use");
+    localStorage.removeItem("location");
+  };
+
   const registerUser = async (currentUser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
@@ -56,7 +72,8 @@ const AppProvider = ({ children }) => {
           location,
         },
       });
-      // local storage later
+      // local storage
+      addUserToLocalStorage({ user, token, location });
     } catch (error) {
       // console.log(error.response);
       dispatch({
