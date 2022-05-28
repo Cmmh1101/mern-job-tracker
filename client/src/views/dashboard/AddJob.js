c6e09b41b6ffc9edeb324c7ecf680256f629ebb7
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-05-27 23:24:00 -0500

    type job select

--- client/src/views/dashboard/AddJob.js
+++ client/src/views/dashboard/AddJob.js
@@ -1,8 +1,100 @@
+import { FormRow, Alert } from "../../components";
+import { useAppContext } from "../../context/appContext";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+
 const AddJob = () => {
+  const {
+    showAlert,
+    displayAlert,
+    position,
+    jobLocation,
+    company,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+  } = useAppContext();
+
+  const handleJobInput = (e) => {
+    e.preventDefault();
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name}: ${value}`);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!position || !company || !jobLocation) {
+      displayAlert();
+      return;
+    }
+
+    console.log("create job");
+  };
+
   return (
-    <div>
-      <h1>Add job</h1>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <h3>{isEditing ? "edit job" : "add job"}</h3>
+        {showAlert && <Alert />}
+        <div className="form-center">
+          {/* position */}
+          <FormRow
+            type="text"
+            name="position"
+            value={position}
+            handleChange={handleJobInput}
+          />
+          {/* company */}
+          <FormRow
+            type="text"
+            name="company"
+            value={company}
+            handleChange={handleJobInput}
+          />
+          {/* job location */}
+          <FormRow
+            labelText="job location"
+            type="text"
+            name="jobLocation"
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+          {/* job type*/}
+          <div className="form-row">
+            <label htmlFor="jobType" className="form-label">
+              Job Type
+            </label>
+            <select
+              name="jobType"
+              value={jobType}
+              onChange={handleJobInput}
+              className="form-select"
+            >
+              {jobTypeOptions.map((itemValue, index) => {
+                return (
+                  <option key={index} value={itemValue}>
+                    {itemValue}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          {/* job type*/}
+          <div className="btn-container">
+            <button
+              type="submit"
+              className="btn btn-block submit-btn"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
   );
 };
 
