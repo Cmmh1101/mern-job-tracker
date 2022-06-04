eb51ff3a3868d7595740c48be7d7f51e1754901c
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-06-04 09:41:57 -0500

    charts completed

--- client/src/components/AreaChart.js
+++ client/src/components/AreaChart.js
@@ -1,7 +1,32 @@
 import React from "react";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
 
-const AreaChart = () => {
-  return <div>area chart</div>;
+const AreaChartComponent = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3 " />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="count"
+          stroke="#2cb1bc"
+          fill="#bef8fd"
+          barSize={75}
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 };
 
-export default AreaChart;
+export default AreaChartComponent;
