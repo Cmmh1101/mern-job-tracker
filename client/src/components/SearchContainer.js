e19e81c62467d771256ff522cf63e97db39c5e73
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-06-08 08:02:08 -0500

    filters completed and initial pagination

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,33 +1,35 @@
-import React from "react";
+import { useEffect } from "react";
 import { FormRow, FormRowSelect } from ".";
 import { useAppContext } from "../context/appContext";
 import Wrapper from "../assets/wrappers/SearchContainer";
 
 const SearchContainer = () => {
   const {
+    getJobs,
     isLoading,
     search,
+    handleChange,
     searchStatus,
-    searchType,
-    sort,
-    sortOptions,
     statusOptions,
     jobTypeOptions,
-    handleChange,
+    searchType,
     clearFilters,
+    sort,
+    sortOptions,
   } = useAppContext();
 
   const handleSearch = (e) => {
     if (isLoading) return;
     handleChange({ name: e.target.name, value: e.target.value });
-    console.log(e.target.name, "name");
-    console.log(e.target.value, "value");
   };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     clearFilters();
   };
+
+  useEffect(() => {
+    getJobs();
+  }, [search, searchStatus, searchType, sort]);
 
   return (
     <Wrapper>
@@ -40,7 +42,7 @@ const SearchContainer = () => {
             name="search"
             value={search}
             handleChange={handleSearch}
-          />
+          ></FormRow>
           {/* search by status */}
           <FormRowSelect
             labelText="job status"
@@ -48,28 +50,30 @@ const SearchContainer = () => {
             value={searchStatus}
             handleChange={handleSearch}
             list={["all", ...statusOptions]}
-          />
+          ></FormRowSelect>
           {/* search by type */}
+
           <FormRowSelect
             labelText="job type"
             name="searchType"
             value={searchType}
             handleChange={handleSearch}
             list={["all", ...jobTypeOptions]}
-          />
+          ></FormRowSelect>
           {/* sort */}
+
           <FormRowSelect
             name="sort"
             value={sort}
             handleChange={handleSearch}
             list={sortOptions}
-          />
+          ></FormRowSelect>
           <button
             className="btn btn-block btn-danger"
             disabled={isLoading}
             onClick={handleSubmit}
           >
-            Clear Filters
+            clear filters
           </button>
         </div>
       </form>
--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -28,6 +28,7 @@ import {
   EDIT_JOB_ERROR,
   SHOW_STATS_BEGIN,
   SHOW_STATS_SUCCESS,
+  CLEAR_FILTERS,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -220,7 +221,12 @@ const AppProvider = ({ children }) => {
   };
 
   const getJobs = async () => {
-    let url = `/jobs`;
+    const { search, searchStatus, searchType, sort } = state;
+
+    let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}`;
+    if (search) {
+      url = url + `&search=${search}`;
+    }
 
     dispatch({ type: GET_JOBS_BEGIN });
     try {
@@ -304,7 +310,7 @@ const AppProvider = ({ children }) => {
   }, []);
 
   const clearFilters = () => {
-    console.log("clear filters");
+    dispatch({ type: CLEAR_FILTERS });
   };
 
   return (
